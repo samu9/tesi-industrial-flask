ad955462aa691ae16f9c6b558d62b1316e141be6
Samuele <s.fusaro@geniodiligence.it>
2021-03-10 22:30:25 +0100

    added pool recycle to create_engine

--- industrial/database/db.py
+++ industrial/database/db.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("mysql+pymysql://root:industrial@localhost:3306/industrial", convert_unicode=False)
+engine = create_engine("mysql+pymysql://root:industrial@localhost:3306/industrial", convert_unicode=False, pool_recycle=1800)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
