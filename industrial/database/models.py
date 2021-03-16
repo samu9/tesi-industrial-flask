241485fcf2cf6d5dee0e61e85f3a2cb00af79125
Samuele <s.fusaro@geniodiligence.it>
2021-03-17 00:41:39 +0100

    added user and machinelog models

--- industrial/database/models.py
+++ industrial/database/models.py
@@ -6,6 +6,13 @@ from sqlalchemy.orm import relationship
 from industrial.database.db import Base
 from industrial.constants import MACHINE_STOP
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), unique=True, nullable=False)
+    log = relationship("MachineLog", back_populates="user")
+
 class Area(Base):
     __tablename__ = "area"
 
@@ -34,6 +41,7 @@ class Machine(Base):
     sector_id = Column(Integer, ForeignKey("sector.id"), nullable=False)
     sector = relationship("Sector", back_populates="machines")
     data = relationship("MachineData", back_populates="machine")
+    log = relationship("MachineLog", back_populates="machine")
     started = Column(Boolean, default=False, nullable=False)
     status = Column(String(5), default=MACHINE_STOP, nullable=False)
 
@@ -46,5 +54,15 @@ class MachineData(Base):
     value1 = Column(Integer, nullable=False, default=0)
     value2 = Column(Integer, nullable=False, default=0)
     value3 = Column(Integer, nullable=False, default=0)
-    timestamp = Column(DateTime, nullable=False, default=datetime.datetime.now())
+    timestamp = Column(DateTime, nullable=False)
 
+class MachineLog(Base):
+    __tablename__ = "machineLog"
+
+    id = Column(Integer, primary_key=True)
+    machine_id = Column(Integer, ForeignKey("machine.id"), nullable=False)
+    machine = relationship("Machine", back_populates="log")
+    action = Column(String(30))
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user = relationship("User", back_populates="log")
+    timestamp = Column(DateTime, nullable=False)
