5772cc8ba75e38caa68b4a792a742412217134c6
Samuele <s.fusaro@geniodiligence.it>
2021-03-18 18:57:21 +0100

    position and steps constants

--- industrial/constants.py
+++ industrial/constants.py
@@ -3,7 +3,32 @@ MACHINE_STOP = "STOP"
 MACHINE_ON = "ON"
 MACHINE_PAUSE = "PAUSE"
 MACHINE_HALT = "HALT"
-MACHINE_RUN = "RUN"
+MACHINE_START = "START"
 
 # SIMULATION
 SIM_SLEEP_TIME = 5
+
+
+POS_RPM = 0
+POS_EFFICIENCY = 1
+POS_TEMP = 2
+
+# VALUES
+
+RESOLVE_STEPS = [
+    {
+        "Check the values."
+    },
+    {
+        "Tune the power know accordingly."
+    },
+    {
+        "Pause the machine."
+    },
+    {
+        "Stop the machine."
+    },
+    {
+        "HALT the machine."
+    }
+]
