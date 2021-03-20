adfe038bc0e68054227408475e3a435967ca8bf4
Samuele <s.fusaro@geniodiligence.it>
2021-03-20 12:15:59 +0100

    fixed run script

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 from industrial import create_app
-from industrial.database import init_db
 
 if __name__== "__main__":
-    init_db()
     app = create_app()
     app.run(debug=True, host='0.0.0.0')
